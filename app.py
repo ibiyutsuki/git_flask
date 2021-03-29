e79743af7a05e97947cc6b86b209a7581f2860bc
ibi yutsuki <y.ibi.sys20@morijyobi.ac.jp>
2021-03-29 10:37:47 +0900

    メニューページの新規作成
    inswx.htmlからの遷移先ページとしてmenu.htmlを作成しました

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def top():
     return render_tempalate("index.html")
 
+@app.route("/menu")
+def menu():
+    return render_tempalate("menu.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
